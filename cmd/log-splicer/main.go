270ec76a6bba5aeab397edb734129b1ebe8e8916
Alexander Cobleigh <cblgh@cblgh.org>
2021-06-21 15:33:29 +0200

    Log splicer rewrite (#9)
    * rewrite log-splicer, now outputs latest seqno as well
    * add review improvements
    * pull up flag parsing to main, pass runtimeArgs struct

--- cmd/log-splicer/main.go
+++ cmd/log-splicer/main.go
@@ -1,5 +1,22 @@
 // SPDX-License-Identifier: MIT
 
+// Tasks the log splicer takes care of during its runtime:
+// * Creates folder super structure, encapsulating each identity in its own folder
+// * Maps identities to folder names they live in
+// * Copies secrets from the fixtures folder to each identity folder
+// * Splics out messages from the one ssb-fixtures log.offset to the many log.offset in said folder structure
+// * Persists an identity mapping of ID to {folderName, latest sequence number} to secret-ids.json
+
+// The log splicer command takes an ssb-fixtures generated folder as input, and a destination folder as output. The
+// destination folder will be populated with identity folders, one folder per identity found in the generated fixtures.
+//
+// Each identity folder contains a log.offset, with the posts created by that identity, and the identity's secret file. The
+// identity folders are named after the filenames of the secrets found in the ssb-fixtures folder, which preserves the
+// pareto distribution of authors (secrets in the lower number ranges have issued more posts).
+//
+// Finally, a mapping from ssb identities @[...].ed25199 to the identity folders is dumped as json to the root of the
+// destination folder. The mapping, in addition to naming the secret folder, also contains an integer count tracking the latest
+// sequence number posted by that identity.
 package main
 
 import (
@@ -21,8 +38,10 @@ import (
 )
 
 type FeedInfo struct {
-	ID  string
-	log margaret.Log
+	ID             string
+	log            margaret.Log
+	latest         int
+	identityFolder string
 }
 
 func inform(e error, message string) error {
@@ -35,7 +54,6 @@ func inform(e error, message string) error {
 
 func mapIdentitiesToSecrets(indir, outdir string, removeExistingLogs bool) (map[string]FeedInfo, error) {
 	feeds := make(map[string]FeedInfo)
-	idsToFolders := make(map[string]string)
 	err := filepath.WalkDir(indir, func(path string, info fs.DirEntry, err error) error {
 		if info.IsDir() {
 			return nil
@@ -53,44 +71,20 @@ func mapIdentitiesToSecrets(indir, outdir string, removeExistingLogs bool) (map[
 				return err
 			}
 
-			// write puppet names based on secret names, to preserve the implicit pareto distribution of post authors
-			// (authors with lower secret ids make more posts)
-			parts := strings.Split(info.Name(), "-")
-			// handle the case where first file is just called "secret"
-			if len(parts) == 1 {
-				parts = append(parts, "0")
-			}
-			n, err := strconv.Atoi(parts[1])
-			if err != nil {
-				return err
-			}
-			foldername := fmt.Sprintf("puppet-%05d", n)
-			// prepare folder paths
-			puppetdir := filepath.Join(outdir, foldername)
-			flumedir := filepath.Join(puppetdir, "flume")
-			logpath := filepath.Join(flumedir, "log.offset")
-			// create correct folder structure
-			err = os.MkdirAll(flumedir, os.ModePerm)
+			puppetname, err := derivePuppetName(info.Name())
 			if err != nil {
 				return err
 			}
 
-			// check if the output log exists
-			info, err := os.Stat(logpath)
-			if err != nil && !os.IsNotExist(err) {
-				return inform(err, fmt.Sprintf("failed to stat output log for %s:", v.ID))
+			v.identityFolder, err = createFolderStructure(outdir, puppetname)
+			if err != nil {
+				return err
 			}
-			// the output log does exist
-			if err == nil && info.Size() > 0 {
-				// -prune was not passed; abort
-				if !removeExistingLogs {
-					return inform(errors.New("-prune was not passed"), "output log already contains data. has the splicer already run?\nsplicer: use -prune to delete pre-existing logs")
-				}
-				// if -prune flag passed -> remove the log before we use it
-				err = os.Remove(logpath)
-				if err != nil {
-					return inform(err, "failed to delete pre-existing output log")
-				}
+
+			logpath := filepath.Join(v.identityFolder, "flume", "log.offset")
+			err = checkLogEmpty(logpath, removeExistingLogs)
+			if err != nil {
+				return err
 			}
 
 			// open a margaret log for the specified output format (lfo)
@@ -98,31 +92,93 @@ func mapIdentitiesToSecrets(indir, outdir string, removeExistingLogs bool) (map[
 			if err != nil {
 				inform(err, fmt.Sprintf("failed to create output log for %s", v.ID))
 			}
+			// save the feed info in the identity mapping
 			feeds[v.ID] = v
-			// map id to the folder containing secret & log
-			idsToFolders[v.ID] = foldername
-			// copy the secret file to the prepared puppet folder
-			err = os.WriteFile(filepath.Join(puppetdir, "secret"), b, 0600)
-			if err != nil {
-				return err
-			}
+
+			err = copySecret(v.identityFolder, b)
+			return err
 		}
 		return nil
 	})
 	if err != nil {
 		return nil, err
 	}
+	return feeds, nil
+}
+
+type FeedJSON struct {
+	Folder string `json:"folder"`
+	Latest int    `json:"latest"`
+}
+
+func persistIdentityMapping(feeds map[string]FeedInfo, outdir string) error {
+	// map id to the identity folder (where the secret + offset.log lives) as well as the feed's latest sequence number
+	idsToFolders := make(map[string]FeedJSON)
+	for id, feedInfo := range feeds {
+		idsToFolders[id] = FeedJSON{Folder: feedInfo.identityFolder, Latest: feedInfo.latest}
+	}
+
 	// write a json blob mapping the identities to the folders containing their secret + log.offset
 	// (we cant use the pubkey ids as folder names since unix does not like base64's charset)
 	b, err := json.MarshalIndent(idsToFolders, "", "  ")
 	if err != nil {
-		return nil, err
+		return err
 	}
 	err = os.WriteFile(filepath.Join(outdir, "secret-ids.json"), b, 0644)
-	if err != nil {
-		return nil, err
+	return err
+}
+
+func copySecret(identityFolder string, b []byte) error {
+	newSecretPath := filepath.Join(identityFolder, "secret")
+	// copy the secret file to the prepared puppet folder
+	err := os.WriteFile(newSecretPath, b, 0600)
+	return err
+}
+
+func derivePuppetName(secretFilename string) (string, error) {
+	// write puppet names based on secret names, to preserve the implicit pareto distribution of post authors
+	// (authors with lower secret ids make more posts)
+	parts := strings.Split(secretFilename, "-")
+	// handle the case where first file is just called "secret"
+	if len(parts) == 1 {
+		parts = append(parts, "0")
 	}
-	return feeds, nil
+	n, err := strconv.Atoi(parts[1])
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("puppet-%05d", n), nil
+}
+
+// Returns the folder the spliced out identity lives in, and an error if os.MkdirAll failed
+func createFolderStructure(outdir, puppetName string) (string, error) {
+	// prepare folder path
+	identityFolder := filepath.Join(outdir, puppetName)
+	// create correct folder structure
+	err := os.MkdirAll(filepath.Join(identityFolder, "flume"), os.ModePerm)
+	return identityFolder, err
+}
+
+func checkLogEmpty(logpath string, removeExistingLogs bool) error {
+	// check if the output log exists
+	info, err := os.Stat(logpath)
+	if err != nil && !os.IsNotExist(err) {
+		return inform(err, fmt.Sprintf("failed to stat output log"))
+	}
+	// the output log does exist
+	if err == nil && info.Size() > 0 {
+		// -prune was not passed; abort
+		if !removeExistingLogs {
+			msg := fmt.Sprintf("output log already contains data. has the splicer already run?\n%s: use -prune to delete pre-existing logs", getToolName())
+			return inform(errors.New("-prune was not passed"), msg)
+		}
+		// if -prune flag passed -> remove the log before we use it
+		err = os.Remove(logpath)
+		if err != nil {
+			return inform(err, "failed to delete pre-existing output log")
+		}
+	}
+	return nil
 }
 
 /*
@@ -133,57 +189,46 @@ func mapIdentitiesToSecrets(indir, outdir string, removeExistingLogs bool) (map[
 * 4. finally, create folders for each author, using the author's pubkey as directory name, and dump an lfo
 * version of their log.offset representation. inside each folder, dump the correct secret as well
  */
-func main() {
-	var verbose bool
-	flag.BoolVar(&verbose, "v", false, "verbose: talks a bit more than than the tool otherwise is inclined to do")
-	var dryRun bool
-	flag.BoolVar(&dryRun, "dry", false, "only output what it would do")
-	var prune bool
-	flag.BoolVar(&prune, "prune", false, "removes existing output logs before writing to them (if -prune omitted, the splicer will instead exit with an error)")
-	var limit int
-	flag.IntVar(&limit, "limit", -1, "how many entries to copy (defaults to unlimited)")
-	flag.Parse()
 
-	logPaths := flag.Args()
-	if len(logPaths) != 2 {
+func spliceLogs(args runtimeArgs) error {
+
+	var err error
+	var input margaret.Log
+
+	logpaths := flag.Args()
+	if len(logpaths) != 2 {
 		cmdName := os.Args[0]
-		fmt.Fprintf(os.Stderr, "Usage: %s <options> <path to ssb-fixtures folder> <output path>\nOptions:\n", cmdName)
+		err := fmt.Errorf("Usage: %s <options> <path to ssb-fixtures folder> <output path>\nOptions:\n", cmdName)
 		flag.PrintDefaults()
-		os.Exit(1)
+		return err
 	}
+	indir, outdir := logpaths[0], logpaths[1]
 
-	if dryRun || verbose {
-		fmt.Fprintf(os.Stderr, "splicer: will read log.offset from %s and output to %s\n", logPaths[0], logPaths[1])
-		if dryRun {
-			return
+	if args.dryRun || args.verbose {
+		fmt.Fprintf(os.Stderr, "%s: will read log.offset from %s and output to %s\n", getToolName(), indir, outdir)
+		if args.dryRun {
+			return nil
 		}
 	}
 
-	var (
-		err   error
-		input margaret.Log
-	)
-
-	sourceFile := filepath.Join(logPaths[0], "flume", "log.offset")
+	sourceFile := filepath.Join(indir, "flume", "log.offset")
 	input, err = openLog(sourceFile)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to open input log %s: %s\n", logPaths[0], err)
-		os.Exit(1)
-	}
-	feeds, err := mapIdentitiesToSecrets(logPaths[0], logPaths[1], prune)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err)
-		os.Exit(1)
+		return fmt.Errorf("failed to open input log %s: %w\n", indir, err)
 	}
 
-	if verbose {
+	feeds, err := mapIdentitiesToSecrets(indir, outdir, args.prune)
+	if err != nil {
+		return err
+	}
+
+	if args.verbose {
 		fmt.Fprintf(os.Stderr, "fixture had %d feeds\n", len(feeds))
 	}
 
-	src, err := input.Query(margaret.Limit(limit))
+	src, err := input.Query(margaret.Limit(args.limit))
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to create query on input log %s: %s\n", logPaths[0], err)
-		os.Exit(1)
+		return fmt.Errorf("failed to create query on input log %s: %w\n", indir, err)
 	}
 
 	i := 0
@@ -194,8 +239,7 @@ func main() {
 			if luigi.IsEOS(err) {
 				break
 			}
-			fmt.Fprintf(os.Stderr, "failed to get log entry %s: %s\n", logPaths[0], err)
-			os.Exit(1)
+			return fmt.Errorf("failed to get log entry %s: %w\n", indir, err)
 		}
 
 		msg := v.(lfoMessage)
@@ -204,26 +248,58 @@ func main() {
 		if !has {
 			continue
 		}
+		a.latest += 1
+		feeds[msg.author.Ref()] = a
 
 		_, err = a.log.Append(v)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to write entry to output log %s: %s\n", logPaths[1], err)
-			os.Exit(1)
+			return fmt.Errorf("failed to write entry to output log %s: %w\n", outdir, err)
 		}
 		i++
 	}
 
-	if verbose {
+	err = persistIdentityMapping(feeds, outdir)
+	if err != nil {
+		return err
+	}
+
+	if args.verbose {
 		fmt.Fprintln(os.Stderr, "all done. closing output log. Copied:", i)
 	}
 
 	for _, a := range feeds {
 		if c, ok := a.log.(io.Closer); ok {
 			if err = c.Close(); err != nil {
-				fmt.Fprintf(os.Stderr, "failed to close output log %s: %s\n", logPaths[1], err)
+				return fmt.Errorf("failed to close output log %s: %w\n", outdir, err)
 			}
 		}
 	}
+	return nil
+}
+
+type runtimeArgs struct {
+	verbose bool
+	dryRun  bool
+	prune   bool
+	limit   int
+}
+
+func main() {
+	args := runtimeArgs{}
+	flag.BoolVar(&args.verbose, "v", false, "verbose: talks a bit more than than the tool otherwise is inclined to do")
+	flag.BoolVar(&args.dryRun, "dry", false, "only output what it would do")
+	flag.BoolVar(&args.prune, "prune", false, "removes existing output logs before writing to them (if -prune omitted, the splicer will instead exit with an error)")
+	flag.IntVar(&args.limit, "limit", -1, "how many entries to copy (defaults to unlimited)")
+	flag.Parse()
+	err := spliceLogs(args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s\n", getToolName(), err)
+		os.Exit(1)
+	}
+}
+
+func getToolName() string {
+	return os.Args[0]
 }
 
 func openLog(path string) (margaret.Log, error) {
