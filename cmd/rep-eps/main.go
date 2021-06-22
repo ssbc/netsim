a68111497a5fe4a1c9cb3e3d2a7a2a305d73ee0e
cblgh <cblgh@cblgh.org>
2021-06-22 17:17:59 +0200

    refactor: break out runtime args, add --out

--- cmd/rep-eps/main.go
+++ cmd/rep-eps/main.go
@@ -21,14 +21,14 @@ type peer struct {
 	blocked map[string]bool
 }
 
-func makePeer(id string) peer {
+func makePeer(args runtimeArgs, id string) peer {
 	p := peer{id: id}
-	p.hops = make([][]string, MAX_HOPS+1)
+	p.hops = make([][]string, args.maxHops+1)
 	p.blocked = make(map[string]bool)
 	return p
 }
 
-func populateHopsAt(count int, peers map[string]peer) {
+func populateHopsAt(args runtimeArgs, count int, peers map[string]peer) {
 	for _, my := range peers {
 		for _, friendId := range my.hops[count-1] {
 			friend := peers[friendId]
@@ -37,8 +37,7 @@ func populateHopsAt(count int, peers map[string]peer) {
 			}
 			for _, hopsFollow := range friend.hops[count-1] {
 				// don't add blocked peers to hops
-				// TODO: toggle this behaviour with flag?
-				if _, exists := my.blocked[hopsFollow]; exists && !REPLICATE_BLOCKED {
+				if _, exists := my.blocked[hopsFollow]; exists && !args.replicateBlocked {
 					continue
 				}
 				my.hops[count] = append(my.hops[count], hopsFollow)
@@ -48,11 +47,11 @@ func populateHopsAt(count int, peers map[string]peer) {
 }
 
 // TODO: should we include hops[0]? i.e. the peer we are inspecting
-func collapse(peers map[string]peer) {
+func collapse(args runtimeArgs, peers map[string]peer) {
 	// prune out duplicates when collapsing the map
 	collapsedHops := make(map[string]map[string]bool)
 	for id, p := range peers {
-		for i := 0; i <= MAX_HOPS; i++ {
+		for i := 0; i <= args.maxHops; i++ {
 			collapsedHops[id] = make(map[string]bool)
 			for _, otherId := range p.hops[i] {
 				collapsedHops[id][otherId] = true
@@ -71,23 +70,15 @@ func collapse(peers map[string]peer) {
 	// persist to disk
 	b, err := json.MarshalIndent(outputMap, "", "  ")
 	check(err)
-	err = os.WriteFile("./expectations.json", b, 0666)
+	err = os.WriteFile(pathAndFile(args.outpath, "expectations.json"), b, 0666)
 	check(err)
 }
 
-var MAX_HOPS int
-var REPLICATE_BLOCKED bool
+func pathAndFile(dirpath, name string) string {
+	return path.Join(path.Dir(dirpath), name)
+}
 
-func main() {
-	flag.IntVar(&MAX_HOPS, "hops", 3, "the default global hops setting")
-	flag.BoolVar(&REPLICATE_BLOCKED, "replicate-blocked", false, "if flag is present, blocked peers will be replicated")
-	flag.Parse()
-
-	if len(flag.Args()) == 0 {
-		fmt.Println("usage: expectations <path to fixtures folder>")
-	}
-
-	graphpath := path.Join(flag.Args()[0], "follow-graph.json")
+func produceExpectations(args runtimeArgs, graphpath string) {
 	b, err := os.ReadFile(graphpath)
 	check(err)
 
@@ -101,7 +92,7 @@ func main() {
 	// false => peer is blocked
 	peers := make(map[string]peer)
 	for id, relations := range v {
-		p := makePeer(id)
+		p := makePeer(args, id)
 		p.hops[0] = append(p.hops[0], id)
 		for relationId, status := range relations {
 			if followed, ok := status.(bool); ok {
@@ -117,10 +108,32 @@ func main() {
 		peers[id] = p
 	}
 
-	if MAX_HOPS >= 2 {
-		for i := 2; i <= MAX_HOPS; i++ {
-			populateHopsAt(i, peers)
+	if args.maxHops >= 2 {
+		for i := 2; i <= args.maxHops; i++ {
+			populateHopsAt(args, i, peers)
 		}
 	}
-	collapse(peers)
+	collapse(args, peers)
+}
+
+type runtimeArgs struct {
+	maxHops          int
+	replicateBlocked bool
+	outpath          string
+}
+
+func main() {
+	var args runtimeArgs
+	flag.IntVar(&args.maxHops, "hops", 3, "the default global hops setting")
+	flag.BoolVar(&args.replicateBlocked, "replicate-blocked", false, "if flag is present, blocked peers will be replicated")
+	flag.StringVar(&args.outpath, "out", "./expectations.json", "the filename and path where the expectations will be dumped")
+	flag.Parse()
+
+	if len(flag.Args()) == 0 {
+		fmt.Println("usage:\n  expectations <flags> <path to fixtures folder>")
+		os.Exit(0)
+	}
+
+	graphpath := pathAndFile(flag.Args()[0], "follow-graph.json")
+	produceExpectations(args, graphpath)
 }
