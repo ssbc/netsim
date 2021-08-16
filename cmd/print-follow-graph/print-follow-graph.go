14b470d25637dc00c8e2403ad35f2e59e08086ed
cblgh <cblgh@cblgh.org>
2021-08-16 16:19:42 +0200

    comment & improve ux of follow graph util

--- cmd/print-follow-graph/print-follow-graph.go
+++ cmd/print-follow-graph/print-follow-graph.go
@@ -21,10 +21,17 @@ func check(err error) {
 
 func main() {
 	var args generation.Args
-	flag.StringVar(&args.FixturesRoot, "fixtures", "./fixtures-output", "root folder containing spliced out ssb-fixtures")
 	flag.IntVar(&args.MaxHops, "hops", 2, "the max hops count to use")
 	flag.IntVar(&args.FocusedCount, "focused", 2, "number of puppets to use for focus group (i.e. # of puppets that verify they are replicating others)")
 	flag.Parse()
+	if len(flag.Args()) == 0 {
+		fmt.Printf("print-follow-graph <options> path-to-spliced-fixtures\n")
+		fmt.Println("prints the follow graph described by a spliced out fixtures folder (netsim generate output), and its replication expectations")
+		fmt.Println("Options:")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	args.FixturesRoot = flag.Args()[0]
 
 	var err error
 	g := generation.Generator{Args: args, Output: os.Stdout}
@@ -38,13 +45,15 @@ func main() {
 
 	puppetNames := make([]string, 0, len(g.IDsToNames))
 	g.NamesToIDs = make(map[string]string)
+	// map puppet names to their ids with g.NamesToIDs
 	for id, secretFolder := range g.IDsToNames {
 		g.NamesToIDs[secretFolder] = id
 		puppetNames = append(puppetNames, g.IDsToNames[id])
 	}
 	sort.Strings(puppetNames)
 
-	// the cohort of peers we care about; the ones who will be issuing `has` stmts, the ones whose data we will inspect
+	// g.FocusGroup is the cohort of peers we care about; the ones who will be issuing `has` stmts, the ones whose data we
+	// will inspect
 	g.FocusGroup = make([]string, args.FocusedCount)
 	for i := 0; i < args.FocusedCount; i++ {
 		g.FocusGroup[i] = fmt.Sprintf("puppet-%05d", i)
@@ -61,10 +70,10 @@ func main() {
 
 			 ========================
 			  start start start start
-			v hops 3 connect hops 2 v
-			v hops 2 connect hops 1 v
-			v hops 1 connect focus  v
-			  done done  done done
+			v hops 3 > connect > hops 2 v
+			v hops 2 > connect > hops 1 v
+			v hops 1 > connect > focus  v
+			  done done done done done
 			 ========================
 	*/
 	focusIds := g.GetIDs(g.FocusGroup)
